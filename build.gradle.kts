310e5f34f6866c364b3b48ae88091812129b4a1f
markm001 <markchrim@gmail.com>
2025-01-10 00:12:39 +0200

    package deployment added

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,19 +30,14 @@ tasks.test {
 }
 
 publishing {
-//    publications.create<MavenPublication>("libs") {
-//        groupId = "com.ccat"
-//        artifactId = "c4tclient"
-//        version = "1.0"
-//
-//        from(components["java"])
-//    }
-//    repositories.maven("/tmp/utils")
-
-    repositories.maven("https://maven.pkg.github.com/markm001/CatClient") {
-        credentials {
-            username = System.getenv("USER")
-            password = System.getenv("TOKEN")
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = "https://maven.pkg.github.com/markm001/CatClient"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
