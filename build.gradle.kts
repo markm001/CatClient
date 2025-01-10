15e665d0cf660579b91ea67e1b43c02d36b4c5cd
markm001 <markchrim@gmail.com>
2025-01-10 11:27:58 +0200

    package deployment added

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = "https://maven.pkg.github.com/markm001/CatClient"
+            url = uri("https://maven.pkg.github.com/markm001/CatClient")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
